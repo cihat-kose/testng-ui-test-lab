b90de61e0e90ddf2950ec0ffca4f93ad3d634c23
Cihat Köse <cihat.kose@hotmail.com>
2025-05-10 15:17:46 +0200

    Add TestNG introduction summary and basic example

---
+++ src/_01_TestNGIntro/TestNGBasicExample.java
@@ -0,0 +1,18 @@
+package _01_TestNGIntro;
+
+import org.testng.annotations.Test;
+
+public class TestNGBasicExample {
+
+    // A simple class to understand the fundamentals of TestNG.
+
+    @Test
+    public void testMethod1() {
+        System.out.println("TestNG Method 1 executed!");  // Demonstrates how a TestNG method works.
+    }
+
+    @Test
+    public void testMethod2() {
+        System.out.println("TestNG Method 2 executed!");  // Shows that you can define and run multiple test methods in TestNG.
+    }
+}
